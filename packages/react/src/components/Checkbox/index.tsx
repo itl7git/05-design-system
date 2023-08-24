b7050a49d74e842c5aa7cbc1254824b8a6f392c7
Italo Leal <itlcoorporativo@gmail.com>
2023-08-24 17:26:15 -0300

    Component: Checkbox

---
+++ packages/react/src/components/Checkbox/index.tsx
@@ -0,0 +1,18 @@
+import { Check } from '@phosphor-icons/react'
+import { CheckboxContainer, CheckboxIndicator } from './styles'
+import { ComponentProps, ElementType } from 'react'
+
+export interface CheckboxProps
+  extends ComponentProps<typeof CheckboxContainer> {
+  as?: ElementType
+}
+
+export function Checkbox(props?: CheckboxProps) {
+  return (
+    <CheckboxContainer {...props}>
+      <CheckboxIndicator asChild>
+        <Check weight="bold" />
+      </CheckboxIndicator>
+    </CheckboxContainer>
+  )
+}
