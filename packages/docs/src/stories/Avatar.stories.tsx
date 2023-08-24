e02af2cee847e88a2b0f2b406fb656b0e36cdffe
Italo Leal <itlcoorporativo@gmail.com>
2023-08-24 20:34:05 -0300

    Controle nos componentes

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -8,6 +8,13 @@ export default {
     src: 'https://github.com/itl7git.png',
     alt: 'Italo Leal',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
--- packages/docs/src/stories/Text.stories.tsx
+++ packages/docs/src/stories/Text.stories.tsx
@@ -8,6 +8,28 @@ export default {
     children:
       'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium, eligendi nulla. Iure natus praesentium dolores. Voluptas mollitia obcaecati, id placeat earum dicta voluptates dolorem quasi suscipit a ipsum, labore nobis?',
   },
+  argTypes: {
+    size: {
+      options: [
+        'xxs',
+        'xs',
+        'sm',
+        'md',
+        'lg',
+        'xl',
+        '2xl',
+        '4xl',
+        '5xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl',
+      ],
+      control: {
+        type: 'inline-radio',
+      },
+    },
+  },
 } as Meta<TextProps>
 
 export const Primary: StoryObj<TextProps> = {}
